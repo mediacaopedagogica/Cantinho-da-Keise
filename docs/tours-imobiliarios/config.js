c8c77d675f4ba2426fae7b2619cebceab0e800c4
mediacaopedagogica <keisepamela.mediadorapedagogica@gmail.com>
2026-08-25 14:03:19 -0400

    Configura banco compartilhado da atividade de tours imobiliários

---
+++ docs/tours-imobiliarios/config.js
@@ -0,0 +1,6 @@
+window.FORUM_CONFIG = {
+  supabaseUrl: "https://stsetmvuacvogbcolouo.supabase.co",
+  anonKey: "sb_publishable_wrULbAYnACY62_ziencB5w_nfRqPoox",
+  activityKey: "mercado-imobiliario-tours-utd-2025",
+  refreshMs: 5000
+};
